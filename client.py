bd98a4fc2d6f79e23a6af439477b21c8ede526ad
Jcseeco <dontbirdanyone@gmail.com>
2023-11-19 07:52:03 +0800

    feat: add args and submit interval

--- client.py
+++ client.py
@@ -2,33 +2,54 @@ import requests
 import random
 from datetime import datetime
 import time
-import json
-
-host = 'http://10.10.2.100:8000'
+import argparse
 
 def measure()->dict:
     return {
         "voltages": [random.randint(3400, 3450) for _ in range(10)],
         "temperatures": [random.randint(250, 280) for _ in range(5)],
-        "timestamp": datetime.now().isoformat()
+        "timestamp": datetime.utcnow().isoformat()
     }
         
 
-def submit(host: str, data):
+def submit(url: str, data):
     try:
-        response = requests.post(host,json=data)
+        response = requests.post(url,json=data,headers=headers)
         print(response)
     except requests.RequestException as e:
         print(e)
         
+def define_args():
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("-dst",dest="dst_url",default="http://hooke:8000",
+                        help="destination full url")
+    parser.add_argument("-t",dest="interval",default=1,
+                        type=int,
+                        help="interval in seconds")
+    parser.add_argument("-l",dest="limit",default=60,
+                        type=int,
+                        help="limit of requests sent")
+    
+    return parser
+
 def main():
+    parser = define_args()
+    args = parser.parse_args()
+    
     count = 0
     while True:
         count +=1
-        if count > 10:
+        if count > args.limit:
             break
-        # print(json.dumps(measure()))
-        submit(host,measure())
-        time.sleep(1)
         
+        submit(args.dst_url,measure())
+        time.sleep(args.interval)
+        
+
+headers ={
+    "Content-Type":"application/json",
+    "Preprocess":"minmax"
+}
+
 main()
